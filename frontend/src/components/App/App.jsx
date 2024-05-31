70844dc5e713b067d53fc2afc6632565bd047880
Elena <emkhuzhina@gmail.com>
2024-06-01 02:32:09 +0530

    use Routes

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Routes, Route, ProtectedRoute } from 'react-router-dom';
+
+import Chat from  '../../pages/Chat/Chat.jsx';
+import Login from  '../../pages/Login/Login.jsx';
+import SignUp from  '../../pages/SignUp/SignUp.jsx';
+import NotFound from  '../../pages/NotFound/NotFound.jsx';
+
 import './App.css';
 
-const App = () => <h1>Hexlet Chat</h1>;
+const App = () => {
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route 
+             path="/" 
+             element={
+                <Chat  />
+             }
+             >
+               
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      );
+}
 
 export default App;
