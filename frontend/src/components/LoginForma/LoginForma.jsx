52eced05a231485f54c011c0270098387c775026
Elena <emkhuzhina@gmail.com>
2024-06-03 01:22:18 +0530

    add main components and styles

---
+++ frontend/src/components/LoginForma/LoginForma.jsx
@@ -0,0 +1,46 @@
+import { Formik, Form, Field } from 'formik';
+
+const LoginForma = () => {
+    const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+    return (
+        <Formik 
+            initialValues={{
+                username: '',
+                password: '',
+            }}
+            onSubmit={async (values) => {
+                await sleep(500);
+                alert(JSON.stringify(values, null, 2));
+            }}
+        >
+            {({ isSubmitting }) => (
+                    <Form>
+                        <Field
+                            name="username"
+                            id="username"
+                            autocomplete="username"
+                            required
+                            placeholder="Пароль"
+                            type='text'
+                        />
+
+                        <Field
+                            name="password"
+                            id="password"
+                            autocomplete="current-password"
+                            required
+                            placeholder="Ваш ник"
+                            type="password"
+                        />
+
+                        <button type="submit" disabled={isSubmitting}>
+                            Войти
+                        </button>
+                    </Form>
+            )}
+        </Formik>
+    );
+}
+
+export default LoginForma; 
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,21 @@
+import LoginForma from '../../components/LoginForma/LoginForma';
+
+import img from '../../assets/images/login.jpg'; 
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='container_enteringData'>
+        <div className='container_enteringData_img'>
+            <img src={img} alt="registering cat" />
+        </div>
+
+        <div className='forma'>
+          <h1>Войти</h1>
+
+          <LoginForma />
+        </div>
+
+    </div>
   )
 }
 
