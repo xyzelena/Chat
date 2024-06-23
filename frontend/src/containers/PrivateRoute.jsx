279e58ac1f3642f8de0e5cfaa5e805bed11825ce
Elena <emkhuzhina@gmail.com>
2024-06-24 02:58:46 +0530

    change jsx to js

--- frontend/src/containers/PrivateRoute.jsx
+++ frontend/src/containers/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate, useLocation } from 'react-router-dom';
 
-import useAuth from '../hooks/useAuth.jsx';
+import useAuth from '../hooks/useAuth.js';
 
 const PrivateRoute = ({ children }) => {
   const authHandle = useAuth();
--- frontend/src/hooks/useAuth.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import authContext from '../contexts/AuthContext.jsx';
-
-const useAuth = () => useContext(authContext);
-
-export default useAuth;
