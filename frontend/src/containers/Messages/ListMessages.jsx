cab314af3926c3092dbadc294601237cb75d3dc6
Elena <emkhuzhina@gmail.com>
2024-06-26 01:57:36 +0530

    fix show list messages

--- frontend/src/containers/Messages/ListMessages.jsx
+++ frontend/src/containers/Messages/ListMessages.jsx
@@ -3,17 +3,21 @@ import { useGetMessagesQuery } from '../../api/messagesApi.js';
 const ListMessages = () => {
   const { data, error, isLoading, refetch } = useGetMessagesQuery();
   // console.log(data);
+  //{ id: '1', body: 'text message', channelId: '1', username: 'admin }
 
   return (
-    <>
-      <div className="text-break mb-2">
-        <b>admin</b>: msg 1
-      </div>
-      <div className="text-break mb-2">
-        <b>admin</b>: msg 2
-      </div>
-    </>
+    <div>
+      {data?.map((message) => (
+        <div className="text-break mb-2" key={message.id}>
+          <b>{message.username}</b>: {message.body}
+        </div>
+      ))}
+    </div>
   );
 };
 
 export default ListMessages;
+
+// <div className="text-break mb-2">
+// <b>admin</b>: msg 1
+// </div>
