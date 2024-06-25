f9a81f69314bc9e84167c4fb97022b2bde778f59
Elena <emkhuzhina@gmail.com>
2024-06-26 01:51:33 +0530

    fix ul, show list channels

--- frontend/src/containers/Channels/ListChannels.jsx
+++ frontend/src/containers/Channels/ListChannels.jsx
@@ -3,7 +3,8 @@ import { useGetChannelsQuery } from '../../api/channelsApi.js';
 
 const ListChannels = () => {
   const { data, error, isLoading, refetch } = useGetChannelsQuery();
-  console.log(data);
+  // console.log(data);
+  // {id: '1', name: 'general', removable: false}
 
   // useEffect(() => {
   //   if (!isLoading && error) refetch();
@@ -14,41 +15,35 @@ const ListChannels = () => {
       id="channels-box"
       className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
     >
-      <li className="nav-item w-100">
-        <button
-          type="button"
-          className="w-100 rounded-0 text-start btn btn-secondary"
-        >
-          <span className="me-1">#</span>general
-        </button>
-      </li>
-
-      <li className="nav-item w-100">
-        <button type="button" className="w-100 rounded-0 text-start btn">
-          <span className="me-1">#</span>random
-        </button>
-      </li>
-
-      <li className="nav-item w-100">
-        <div role="group" className="d-flex dropdown btn-group">
-          <button
-            type="button"
-            className="w-100 rounded-0 text-start text-truncate btn"
-          >
-            <span className="me-1">#</span>12345
+      {data?.map((channel) => (
+        <li className="nav-item w-100" key={channel.id}>
+          <button type="button" className="w-100 rounded-0 text-start btn">
+            <span className="me-1">#</span>
+            {channel.name}
           </button>
-          <button
-            type="button"
-            id="react-aria5629665839-:r1:"
-            aria-expanded="false"
-            className="flex-grow-0 dropdown-toggle dropdown-toggle-split btn"
-          >
-            <span className="visually-hidden">Управление каналом</span>
-          </button>
-        </div>
-      </li>
+        </li>
+      ))}
     </ul>
   );
 };
 
 export default ListChannels;
+
+/* <li className="nav-item w-100">
+  <div role="group" className="d-flex dropdown btn-group">
+    <button
+      type="button"
+      className="w-100 rounded-0 text-start text-truncate btn"
+    >
+      <span className="me-1">#</span>12345
+    </button>
+    <button
+      type="button"
+      id="react-aria5629665839-:r1:"
+      aria-expanded="false"
+      className="flex-grow-0 dropdown-toggle dropdown-toggle-split btn"
+    >
+      <span className="visually-hidden">Управление каналом</span>
+    </button>
+  </div>
+</li>; */
