e7493b8dc01b6c906af671a1151f4ae50b37a92f
Elena <emkhuzhina@gmail.com>
2024-06-27 01:12:20 +0530

    fix output current messages

--- frontend/src/containers/Messages/ListMessages.jsx
+++ frontend/src/containers/Messages/ListMessages.jsx
@@ -1,15 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { useGetMessagesQuery } from '../../api/messagesApi.js';
+
+import { setCountCurrentMessages } from '../../slices/currentChannelSlice.js';
 
 const ListMessages = () => {
   const { data, error, isLoading, refetch } = useGetMessagesQuery();
   // console.log(data);
   //{ id: '1', body: 'text message', channelId: '1', username: 'admin }
 
-  // channelId: '1'  slice
+  const dispatch = useDispatch();
+
+  const currentChannel = useSelector(
+    (state) => state.currentChannel.currentChannelData,
+  );
+
+  const currentChannelMessages = data?.filter(
+    (message) => message.channelId === currentChannel?.id,
+  );
+
+  useEffect(() => {
+    dispatch(setCountCurrentMessages(currentChannelMessages?.length));
+  });
 
   return (
     <div>
-      {data?.map((message) => (
+      {currentChannelMessages?.map((message) => (
         <div className="text-break mb-2" key={message.id}>
           <b>{message.username}</b>: {message.body}
         </div>
