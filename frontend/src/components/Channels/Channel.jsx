93e5ac9c8e4580af84548f1e0fbb13b079a65942
Elena <emkhuzhina@gmail.com>
2024-06-28 01:57:03 +0530

    move channel

---
+++ frontend/src/components/Channels/Channel.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import cn from 'classnames';
+
+const Channel = (props) => {
+  const { channel, onSelect, currentChannelId } = props;
+
+  const btnClass = cn('w-100', 'rounded-0', 'text-start', 'btn', {
+    'btn-primary': currentChannelId === channel.id,
+  });
+
+  return (
+    <li className="nav-item w-100">
+      <button type="button" className={btnClass} onClick={onSelect}>
+        <span className="me-1">#</span>
+        {channel.name}
+      </button>
+    </li>
+  );
+};
+
+export default Channel;
--- frontend/src/containers/Channels/ListChannels.jsx
+++ frontend/src/containers/Channels/ListChannels.jsx
@@ -1,12 +1,16 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { useGetChannelsQuery } from '../../api/channelsApi.js';
 
 import { setCurrentChannel } from '../../slices/currentChannelSlice.js';
 
+import Channel from '../../components/Channels/Channel.jsx';
+
 const ListChannels = () => {
   const dispatch = useDispatch();
+
+  const { currentChannelData } = useSelector((state) => state.currentChannel);
 
   const { data, error, isLoading, refetch } = useGetChannelsQuery();
   // console.log(data);
@@ -26,16 +30,12 @@ const ListChannels = () => {
       className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
     >
       {data?.map((channel) => (
-        <li className="nav-item w-100" key={channel.id}>
-          <button
-            type="button"
-            className="w-100 rounded-0 text-start btn"
-            onClick={() => dispatch(setCurrentChannel(channel))}
-          >
-            <span className="me-1">#</span>
-            {channel.name}
-          </button>
-        </li>
+        <Channel
+          key={channel.id}
+          currentChannelId={currentChannelData.id ?? 0}
+          channel={channel}
+          onSelect={() => dispatch(setCurrentChannel(channel))}
+        />
       ))}
     </ul>
   );
