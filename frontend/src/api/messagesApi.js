ce467a3bfff6badb51860fd9aa83cc10cb11fd62
Elena <emkhuzhina@gmail.com>
2024-07-05 01:09:53 +0530

    change fetchBaseQuery

--- frontend/src/api/messagesApi.js
+++ frontend/src/api/messagesApi.js
@@ -1,12 +1,25 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
-
-import axiosApi from './axiosApi.js';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
 
 import ROUTES from '../utils/routes.js';
 
+import { getItemStorage } from '../utils/authLocalStorage.js';
+
+const baseUrl = `/api/v1${ROUTES.messages}`;
+
 export const messagesApi = createApi({
   reducerPath: 'messagesApi',
-  baseQuery: () => axiosApi(ROUTES.messages),
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+    prepareHeaders: (headers) => {
+      const userId = getItemStorage();
+
+      if (userId && userId.token) {
+        headers.set('authorization', `Bearer ${userId.token}`);
+      }
+
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getMessages: builder.query({
       query: () => '',
