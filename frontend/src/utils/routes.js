b401a5207ca643299f058a8c659cf894c9b35d45
Elena <emkhuzhina@gmail.com>
2024-07-06 23:57:07 +0530

    add routes

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -1,8 +1,13 @@
 const ROUTES = {
+  home: '/',
   login: '/login',
   signup: '/signup',
   channels: '/channels',
   messages: '/messages',
+  loginApi: '/api/v1/login',
+  signupApi: '/api/v1/signup',
+  channelsApi: '/api/v1/channels',
+  messagesApi: '/api/v1/messages',
 };
 
 export default ROUTES;
