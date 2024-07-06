d524c8e6bebfcd0e24ae3a50efe6bb10784a915f
Elena <emkhuzhina@gmail.com>
2024-07-07 00:35:55 +0530

    add endpoints

--- frontend/src/api/channelsApi.js
+++ frontend/src/api/channelsApi.js
@@ -1,17 +1,57 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
-
-import axiosApi from './axiosApi.js';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import ROUTES from '../utils/routes.js';
 
+import { getItemStorage } from '../utils/authLocalStorage.js';
+
 export const channelsApi = createApi({
   reducerPath: 'channelsApi',
-  baseQuery: () => axiosApi(ROUTES.channels),
+  baseQuery: fetchBaseQuery({
+    baseUrl: ROUTES.channelsApi,
+    prepareHeaders: (headers) => {
+      const userId = getItemStorage();
+
+      if (userId && userId.token) {
+        headers.set('Authorization', `Bearer ${userId.token}`);
+      }
+
+      return headers;
+    },
+  }),
+  tagTypes: ['Channel'],
   endpoints: (builder) => ({
     getChannels: builder.query({
       query: () => '',
+      providesTags: ['Channel'],
+    }),
+    addChannel: builder.mutation({
+      query: (channel) => ({
+        method: 'POST',
+        body: channel,
+      }),
+      invalidatesTags: ['Channel'],
+    }),
+    removeChannel: builder.mutation({
+      query: (id) => ({
+        url: `/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Channel'],
+    }),
+    editChannel: builder.mutation({
+      query: ({ id, ...newChannel }) => ({
+        url: `/${id}`,
+        method: 'PATCH',
+        body: newChannel,
+      }),
+      invalidatesTags: ['Channel'],
     }),
   }),
 });
 
-export const { useGetChannelsQuery } = channelsApi;
+export const {
+  useGetChannelsQuery,
+  useAddChannelMutation,
+  useRemoveChannelMutation,
+  useEditChannelMutation,
+} = channelsApi;
