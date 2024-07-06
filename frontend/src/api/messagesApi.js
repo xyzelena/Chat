923df3420d4b5d8e7857a1c31db9c0b883835636
Elena <emkhuzhina@gmail.com>
2024-07-06 23:57:27 +0530

    fix endpoints

--- frontend/src/api/messagesApi.js
+++ frontend/src/api/messagesApi.js
@@ -1,36 +1,48 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import ROUTES from '../utils/routes.js';
 
 import { getItemStorage } from '../utils/authLocalStorage.js';
 
-const baseUrl = `/api/v1${ROUTES.messages}`;
-
 export const messagesApi = createApi({
   reducerPath: 'messagesApi',
   baseQuery: fetchBaseQuery({
-    baseUrl,
+    baseUrl: ROUTES.messagesApi,
     prepareHeaders: (headers) => {
       const userId = getItemStorage();
 
       if (userId && userId.token) {
-        headers.set('authorization', `Bearer ${userId.token}`);
+        headers.set('Authorization', `Bearer ${userId.token}`);
       }
 
       return headers;
     },
   }),
+  tagTypes: ['Message'],
   endpoints: (builder) => ({
     getMessages: builder.query({
       query: () => '',
+      providesTags: ['Message'],
     }),
     sendMessage: builder.mutation({
       query: (newMessage) => ({
         method: 'POST',
         body: newMessage,
       }),
+      providesTags: ['Message'],
+    }),
+    removeMessage: builder.mutation({
+      query: (id) => ({
+        url: `/${id}`,
+        method: 'DELETE',
+        providesTags: ['Message'],
+      }),
     }),
   }),
 });
 
-export const { useGetMessagesQuery, useSendMessageMutation } = messagesApi;
+export const {
+  useGetMessagesQuery,
+  useSendMessageMutation,
+  useRemoveMessageMutation,
+} = messagesApi;
